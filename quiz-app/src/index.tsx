b3f4594b110671e611e6bfd2ad86a8c1537d1696
saurav darji <sauravdarji8793@gmail.com>
2022-06-14 00:12:54 +0530

    Quiz-Home-Page

--- quiz-app/src/index.tsx
+++ quiz-app/src/index.tsx
@@ -3,13 +3,14 @@ import { render } from "react-dom";
 import { BrowserRouter as Router } from "react-router-dom";
 import "./Styles.css";
 import "./Color/tailwind.css";
-
 import App from "./App";
 
 const rootElement = document.getElementById("root");
-render(
+render( 
+  
     <Router>
       <App />
     </Router>,
+   
   rootElement
 );
