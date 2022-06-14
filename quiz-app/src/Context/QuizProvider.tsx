80220b4fc78bd675598fe2e8aa3010664f0642b6
saurav darji <sauravdarji8793@gmail.com>
2022-06-14 12:14:11 +0530

    Questions-update

--- quiz-app/src/Context/QuizProvider.tsx
+++ quiz-app/src/Context/QuizProvider.tsx
@@ -1,10 +1,11 @@
 import React, { createContext, useReducer, useContext } from "react";
+import { quizReducer } from "../Reducers/QuizReducer";
 import { Quiz } from "../Data/QuizType";
 import { quizOne, quizTwo } from "../Data/GetQuiz";
-
+import { Action } from "../Reducers/QuizReducer";
 
 // type Action = { type: "SET_ANSWER" } | { type: "SHOW_SCORE" };
-
+type Dispatch = (action: Action) => void;
 type StatusState = "starting" | "finished" | "reset";
 
 export type InitialState = {
@@ -15,10 +16,7 @@ export type InitialState = {
   loading: boolean;
 };
 
-// type Action =
-//   | { type: "RESET" }
-//   | { type: "INCREMENT"; payload: { score: number } }
-//   | { type: "DECREMENT"; payload: { negativeScore: number } };
+
 
 type QuizProviderProps = {
   children: React.ReactNode;
@@ -35,11 +33,20 @@ export const quizState: InitialState = {
 
 export const QuizContext = createContext<{
   state: InitialState;
+  dispatch: Dispatch;
 }>({
   state: quizState,
   dispatch: () => undefined
 });
 
+export function QuizProvider({ children }: QuizProviderProps) {
+  const [state, dispatch] = useReducer(quizReducer, quizState);
+  return (
+    <QuizContext.Provider value={{ state, dispatch }}>
+      {children}
+    </QuizContext.Provider>
+  );
+}
 
 export function useQuiz() {
   return useContext(QuizContext);
--- quiz-app/src/Data/QuizType.ts
+++ quiz-app/src/Data/QuizType.ts
@@ -1,8 +1,15 @@
 
+export type Question = {
+  question: string;
+  points: number;
+  answer: number | null;
+  negativePoints?: number;
+};
+
 export type Quiz = {
   id: number;
   quizName: string;
   description: string;
   image: string;
-
+  questions: Question[];
 };
